acd7e5730ffb0ef4fc4fa01b7d4bb91941459837
fernando navajas <fnavajas@buk.cl>
2021-07-25 18:42:37 -0400

    fix: corregir vista de sala cuando se lista la mano de poker , lo elementos de atras quedan ocultos

--- src/components/PokerPot.js
+++ src/components/PokerPot.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import socket from '../socketConfig';
 import Card from './Card';
 
+import './PokerPot.scss';
+
 // Componente PokerPot
 // Muestra el pozo de cartas en la mesa
 class PokerPot extends Component {
@@ -13,10 +15,17 @@ class PokerPot extends Component {
   render() {
     return (
       <div className="PokerPot">
-        <Card number={1} show={false}/>
-        <Card number={2} show={false}/>
-        <Card number={3} show={false}/>
-        <Card number={4} show={false}/>
+        <span className="score-element">
+          <p className="score-text">El promedio es de: 5 </p>
+        </span>
+        <div className="pot">
+          <Card number={1} show={false}/>
+          <Card number={2} show={false}/>
+          <Card number={3} show={false}/>
+          <Card number={4} show={false}/>
+          <Card number={1} show={false}/>
+          <Card number={4} show={false}/>
+        </div>
       </div>
     );
   }
--- src/components/Room.js
+++ src/components/Room.js
@@ -4,6 +4,7 @@ import socket from '../socketConfig';
 import PokerHand from './PokerHand';
 import PokerPot from './PokerPot';
 
+import './Room.scss'
 // Componente room
 // Contiene la logica de una unica sala
 class Room extends Component {
